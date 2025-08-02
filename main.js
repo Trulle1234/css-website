05c45e6c21dab3eb8e98bcea4d32ff945b626f1f
Trulle1234 <leonard.trulsson@gmail.com>
2025-08-02 18:51:03 +0200

    tab

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const cssInput = document.querySelector('.css-input');
 const applyButton = document.querySelector('.apply-css');
-const resetButton = document.querySelector('.reset-css')
+const resetButton = document.querySelector('.reset-css');
 const styleTag = document.getElementById('dynamic-style');
-const defaultValue = cssInput.value
+const defaultValue = cssInput.value;
+
+cssInput.addEventListener('keydown', function(e) {
+    if (e.key === 'Tab') {
+        e.preventDefault();
+        const start = this.selectionStart;
+        const end = this.selectionEnd;
+
+        const spaces = "    ";
+        this.value = this.value.substring(0, start) + spaces + this.value.substring(end);
+        this.selectionStart = this.selectionEnd = start + spaces.length;
+    }
+});
 
 applyButton.addEventListener('click', () => {
     styleTag.textContent = cssInput.value;
 });
 
 resetButton.addEventListener('click', () => {
-    styleTag.textContent = defaultValue
+    styleTag.textContent = defaultValue;
     cssInput.value = defaultValue;
-})
+});
